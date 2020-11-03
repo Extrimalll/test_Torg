805f60e3f576eceb38e6950a1fd601ab4b5877e5
Vlad <yasyrev2010@gmail.com>
2020-11-03 13:40:12 +0500

    Edit sql query!

--- task1/query.sql
+++ task1/query.sql
@@ -1,4 +1,4 @@
-SELECT procedurs.name, tb1.SUMMA AS SUMMA,tb2.quantity AS quantitys
+SELECT procedurs.name, tb1.SUMMA AS SummLots,tb2.quantity AS colPos
 FROM procedurs
 LEFT JOIN (SELECT lots.procedure_id, SUM(lots.price) SUMMA FROM lots GROUP BY procedure_id ) tb1 ON tb1.procedure_id = procedurs.id
 LEFT JOIN (SELECT positions.lot_id, SUM(positions.quantity) quantity FROM positions GROUP BY lot_id) tb2 ON tb2.lot_id = procedurs.id
