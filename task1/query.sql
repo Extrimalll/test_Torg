8517b80c8dd325c3357f13508ea4712f048736d6
Vlad <yasyrev2010@gmail.com>
2020-11-03 20:44:59 +0500

    Edit sql query!

--- task1/query.sql
+++ task1/query.sql
@@ -1,8 +1,8 @@
-SELECT p.name,  lots.priceSum, lots.sumQuant
+SELECT p.name,  lots.SummLots, lots.colPos
 FROM procedurs p
 JOIN
-(SELECT l.id, l.procedure_id, sum(l.price) AS priceSum, sum(pos.sum) as sumQuant
+(SELECT l.id, l.procedure_id, sum(l.price) AS SummLots, sum(pos.sum) as colPos
   FROM lots l
-  LEFT JOIN (SELECT lot_id, sum(quantity) AS sumFROM positions GROUP BY lot_id ) pos ON pos.lot_id = l.id
+  LEFT JOIN ( SELECT lot_id, sum(quantity) AS sum FROM positions GROUP BY lot_id) pos ON pos.lot_id = l.id
   GROUP BY l.procedure_id
 ) lots ON lots.procedure_id = p.id
